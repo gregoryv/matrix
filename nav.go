a6cce35158fe212f6e3887b9a8b3f157fa19f97f
Gregory Vincic <g@7de.se>
2018-02-18 09:58:31 +0100

    Refactor

--- nav.go
+++ nav.go
@@ -11,54 +11,42 @@ func NewXYNavigator(x, y, xi, yi, xj, yj int) *XYNavigator {
 
 // Right returns next position to the right, if the end of a row is reached
 // following position starting at xi is returned.
-func (nav *XYNavigator) Right() (x, y int, more bool) {
+func (nav *XYNavigator) Right() (x, y int, inside bool) {
 	nav.x++
 	if nav.x > nav.xj {
 		// next row
 		nav.y++
 		nav.x = nav.xi
 	}
-	if nav.y > nav.yj {
-		return nav.x, nav.y, false
-	}
-	return nav.x, nav.y, true
+	return nav.x, nav.y, nav.y <= nav.yj
 }
 
-func (nav *XYNavigator) Up() (x, y int, more bool) {
-	nav.y--
-	if nav.y < nav.yi {
-		// previous column
-		nav.x--
-		nav.y = nav.yj
-	}
-	if nav.x < nav.xi {
-		return nav.x, nav.y, false
-	}
-	return nav.x, nav.y, true
-}
-
-func (nav *XYNavigator) Down() (x, y int, more bool) {
-	nav.y++
-	if nav.y > nav.yj {
-		// next column
-		nav.x++
-		nav.y = nav.yi
-	}
-	if nav.x > nav.xj {
-		return nav.x, nav.y, false
-	}
-	return nav.x, nav.y, true
-}
-
-func (nav *XYNavigator) Left() (x, y int, more bool) {
+func (nav *XYNavigator) Left() (x, y int, inside bool) {
 	nav.x--
 	if nav.x < nav.xi {
 		// previous row
 		nav.y--
 		nav.x = nav.xj
 	}
+	return nav.x, nav.y, nav.y >= nav.yi
+}
+
+func (nav *XYNavigator) Up() (x, y int, inside bool) {
+	nav.y--
 	if nav.y < nav.yi {
-		return nav.x, nav.y, false
+		// previous column
+		nav.x--
+		nav.y = nav.yj
 	}
-	return nav.x, nav.y, true
+	return nav.x, nav.y, nav.x >= nav.xi
+}
+
+func (nav *XYNavigator) Down() (x, y int, inside bool) {
+	nav.y++
+	if nav.y > nav.yj {
+		// next column
+		nav.x++
+		nav.y = nav.yi
+	}
+	return nav.x, nav.y, nav.x <= nav.xj
 }
